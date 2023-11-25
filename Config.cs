6b496d7ffe0094952ebc38ace1727455ed8982e5
olimov Amir <olimov.amir@mail.ru>
2023-11-25 18:40:15 +0300

    fix bag

--- Config.cs
+++ Config.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using PRTelegramBot.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
 
 namespace TelegramBot_StoreOlimova
 {
-    internal class Config
+    class Config
     {
+        [ReplyMenuHandler(true, "Привет", "Салом", "hi", "Салам")]
+        public static async Task Excample(ITelegramBotClient botClient, Update update)
+        {
+            // Получаем имя отправителя
+            string senderName = GetFirstName(update.Message.From);
+
+            // Формируем сообщение с именем отправителя
+            var message = $"Добро пожаловать, {senderName}";
+
+            // Отправляем сообщение
+            var senMessage = await PRTelegramBot.Helpers.Message.Send(botClient, update, message);
+        }
+
+        // Метод для получения имени отправителя
+        private static string GetFirstName(User user)
+        {
+            return user.FirstName;
+        }
     }
 }
+
--- Program.cs
+++ Program.cs
@@ -1,1 +1,49 @@
 ﻿
+using PRTelegramBot.Core;
+using System.Data;
+
+const string EXIT_COMMAND = "exit";
+
+// конфигурация бота
+var telegram = new PRBot(option =>
+{
+    option.Token = "6351669134:AAEZN4E-ctWoh8Si8aEzF28sRB9HCa63AJ4";
+    option.ClearUpdatesOnStart = true;
+    option.WhiteListUsers = new List<long>() { };
+    option.Admins = new List<long>() { };
+    option.BotId = 0;
+});
+
+// команда запуск бота
+await telegram.Start();
+
+telegram.OnLogCommon += Telegram_OnLogCommon;
+telegram.OnLogError += Telegram_OnLogError;
+
+void Telegram_OnLogError(Exception err, long? id)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    string errorMsg = $"{DateTime.Now}:{err}";
+    Console.WriteLine(errorMsg);
+    Console.ResetColor();
+}
+
+
+void Telegram_OnLogCommon(string msg, PRBot.TelegramEvents typeEnent, ConsoleColor color)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    string message = $"{DateTime.Now}:{msg}";
+    Console.WriteLine(message);
+    Console.ResetColor();
+}
+
+
+// чтобы программа не закрылась
+while (true)
+{
+    var result = Console.ReadLine();
+    if (result.ToLower() == EXIT_COMMAND)
+    {
+        Environment.Exit(0);
+    }
+}
